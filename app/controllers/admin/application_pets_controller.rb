789fe6f7ff57521a7685e4ddc943327d78ca35af
Huy Phan <huyphan520@gmail.com>
2023-02-15 16:24:56 -0600

    Change status from string to integer

--- app/controllers/admin/application_pets_controller.rb
+++ app/controllers/admin/application_pets_controller.rb
@@ -3,7 +3,7 @@ class Admin::ApplicationPetsController < ApplicationController
   def update
     @application = Application.find(params[:id])
     @application_pet = ApplicationPet.find_by_app_pet(params[:id], params[:pet_id])
-    @application_pet.update(app_pet_status: params[:status])
+    @application_pet.update(app_pet_status: params[:status].to_i)
     redirect_to "/admin/applications/#{@application.id}"
   end
 end
